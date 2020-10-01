8f143bd918e1d3886559678e3529f50cebc5cb2b
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-01 22:37:30 +0300

    Реализация модели представления

---
+++ WPFTest/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WPFTest.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ WPFTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using WPFTest.ViewModels.Base;
+
+namespace WPFTest.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
