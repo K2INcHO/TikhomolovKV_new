2ca5413a40b66efc662373c2112ceba0d45d429f
K2INcHO <k3incho@inbox.ru>
2020-10-02 09:54:43 +0300

    Перенос инфраструктуры

---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.ViewModels.Base;
+
+namespace MailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
