3df0b0dac06b8ef21e42d0d00d9d6ab801b4e1de
K2INcHO <k3incho@inbox.ru>
2020-10-02 11:07:49 +0300

    Модель представления главного окна основного проекта

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,6 +1,27 @@
-﻿namespace MailSender
+﻿using System;
+using MailSender.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MailSender
 {
+    //создаем контейнер сервисов для их последующего использования в приложении
     public partial class App
     {
+        //создаем хостинг, в котором будет "жить" контейнер
+        private static IHost _Hosting;
+
+        //создаем приложение при помощи "построителя хоста"
+        public static IHost Hosting => _Hosting
+            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
---
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MailSender.ViewModels
+{
+    //создаем специальный объект, который предназначен для того чтобы из любой точки разметки
+    //   получить любую ViewModel
+    class ViewModelLocator
+    {
+        //внутри будет набор свойств, каждое из которых представляет свою ViewModel
+        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+
+    }
+}
